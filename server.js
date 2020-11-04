3ee93f9e00cf4c03596b92a8c6816b3e89601a8c
Enariel <56527754+Enariel@users.noreply.github.com>
2020-11-03 19:27:34 -0500

    add login POST route

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 const mockUserData = [
     { name: 'Oliver' },
     { name: 'Connor' }
 ]
+
+//GET requests only returns requests from server
+//POST requests send data to server
+//Post requests come after get requests.
+//A 'login' test function
+
 
 // '/' is a directory within the server root folder/url
 // http://localhost:8000/users
@@ -28,6 +37,35 @@ app.get('/users/:id', function (req, res)
         user: req.params.id
     })
 })
+
+app.post('/login', function (req, res)
+{
+    //Passwords are encrypted and set to database
+    const username = req.body.username;
+    const password = req.body.password;
+
+    //This information usually comes from said database
+    const testUser = "ThreeCats";
+    const testPass = "NamiHasAMustache";
+
+    if (username === testUser && password === testPass)
+    {
+        //a json webtoken method here
+        res.json({
+            success: true,
+            message: 'password and username match!',
+            token: 'Encryption stuff goes here'
+        })
+    }
+    else
+    {
+        res.json({
+            success: false,
+            message: 'Denied',
+        })
+    }
+})
+
 
 //This stays at the bottom
 
